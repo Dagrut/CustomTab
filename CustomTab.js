0bd3b0cb3bd91780bd772b8209dffbe68cfbb2b1
dagrut <dagrut@dagrut.info>
2021-05-14 20:35:17 +0400

    Faster updates of gravatar images Also using # as favicon href

--- CustomTab.js
+++ CustomTab.js
@@ -1,8 +1,9 @@
 (function(global) {
-	const $link = document.querySelector("link[rel='shortcut icon']");
+	const $link = document.querySelector("link[rel='icon']");
 	const $input = document.getElementsByTagName('input')[0];
 	const $body = document.body;
 	let timeoutId = null;
+	let nextFaviconValue = null;
 
 	function pad2(x) {
 		x += '';
@@ -15,33 +16,41 @@
 	}
 
 	function updateFavicon(value) {
-		if (timeoutId) {
-			clearTimeout(timeoutId);
+		const titleEnc = encodeURIComponent(value);
+		const hash = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+		for (let i = 0 ; i < titleEnc.length ; i++) {
+			const k = i % hash.length;
+			hash[k] = (hash[k] + titleEnc.charCodeAt(i)) % 256;
 		}
+		const hashStr = hash.map((x) => pad2(x.toString(16))).join('');
+		const gravatarUrl = `https://www.gravatar.com/avatar/${hashStr}?d=identicon&f=y`;
 
-		timeoutId = setTimeout(function() {
-			timeoutId = null;
+		// Tried to avoid the double image download here, but nothing works... PR welcome!
+		$link.href = gravatarUrl;
+		$body.style.backgroundImage = `url(${gravatarUrl})`;
+	}
 
-			const titleEnc = encodeURIComponent(value);
-			const hash = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-			for (let i = 0 ; i < titleEnc.length ; i++) {
-				const k = i % hash.length;
-				hash[k] = (hash[k] + titleEnc.charCodeAt(i)) % 256;
-			}
-			const hashStr = hash.map((x) => pad2(x.toString(16))).join('');
-			const gravatarUrl = `https://www.gravatar.com/avatar/${hashStr}?d=identicon&f=y`;
-
-			// Tried to avoid the double image download here, but nothing works... PR welcome!
-			$link.href = gravatarUrl;
-			$body.style.backgroundImage = `url(${gravatarUrl})`;
-		}, 1000);
+	function updateFaviconDelayed(value) {
+		if (timeoutId) {
+			nextFaviconValue = value;
+		} else {
+			nextFaviconValue = null;
+			updateFavicon(value);
+			timeoutId = setTimeout(function() {
+				timeoutId = null;
+				if (nextFaviconValue) {
+					updateFavicon(nextFaviconValue);
+					nextFaviconValue = null;
+				}
+			}, 1000);
+		}
 	}
 
 	function inputChange(value) {
 		document.title = value || '---';
 		
 		location.hash = value ? '#' + encodeURIComponent(value) : '';
-		updateFavicon(value || '');
+		updateFaviconDelayed(value || '');
 	}
 
 	function focusInput() {
